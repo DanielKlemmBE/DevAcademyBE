f25829fa9a9edc2603d1a0f7f1c68c01ccd89c0b
Daniel Klemm <daniel.klemm@bearingpoint.com>
2022-09-21 16:06:55 +0200

    S2: Add test

---
+++ src/test/java/dev/academy/MainTest.java
@@ -0,0 +1,15 @@
+package dev.academy;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ServiceConfigurationError;
+
+public class MainTest {
+
+
+    @Test
+    public void a() {
+        System.err.println("hih");
+    }
+}
