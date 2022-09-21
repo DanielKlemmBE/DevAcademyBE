d677398490407565ec437b3c3d07176bde4cc5b7
OzgeanBE <ozgean.mitrof@bearingpoint.com>
2022-09-21 17:26:09 +0300

    S2: Create ConfigHolder singleton example

---
+++ src/test/java/dev/academy/patterns/singleton/ConfigHolderTest.java
@@ -0,0 +1,15 @@
+package dev.academy.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigHolderTest {
+
+    @Test
+    public void getPoolSize() {
+        assertThat(ConfigHolder.getInstance().getPoolSize()).isEqualTo(0);
+        ConfigHolder.getInstance().setPoolSize(25);
+        assertThat(ConfigHolder.getInstance().getPoolSize()).isEqualTo(25);
+    }
+
+}
