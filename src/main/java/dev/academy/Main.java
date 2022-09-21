ac97fc9159ce007431b83380ae70dc302215dd50
OzgeanBE <ozgean.mitrof@bearingpoint.com>
2022-09-21 14:32:55 +0300

    First Git commit

--- src/main/java/dev/academy/Main.java
+++ src/main/java/dev/academy/Main.java
@@ -2,6 +2,6 @@ package dev.academy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("OzgeanBE commit");
     }
 }
